4d8a16120664e00761521ec7980cf1382695c8cb
Daniel <barseghian.daniel@gmail.com>
2026-08-01 18:17:49 +0200

    Added parrying animation (working)

--- Samourai-on-a-budget/Assets/Scripts/Parry.cs
+++ Samourai-on-a-budget/Assets/Scripts/Parry.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,32 +10,28 @@ public class Parry : MonoBehaviour
     public AudioClip parryAudio;
     public AudioSource audioSource;
     public Animator animator;
+    private bool getParry = false;
 
     void Update()
     {
+        Debug.Log($"{getParry}");
         transform.rotation = Quaternion.Euler(orientation.eulerAngles.x, orientation.eulerAngles.y, 0);
 
-        if (parryObject && parryInputAction.WasPressedThisFrame())
+        if (parryInputAction.WasPressedThisFrame() && !getParry)
         {
+            getParry = true;
             animator.SetTrigger("Parry");
-            audioSource.PlayOneShot(parryAudio);
-            Destroy(parryObject);
+
+            StartCoroutine(EndParry());
         }
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Bullet"))
+        if (other.gameObject.CompareTag("Bullet") && getParry)
         {
-            parryObject = other.gameObject;
-        }
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Bullet") && other.gameObject == parryObject)
-        {
-            parryObject = null;
+            audioSource.PlayOneShot(parryAudio);
+            Destroy(parryObject);
         }
     }
 
@@ -46,5 +43,19 @@ public class Parry : MonoBehaviour
     void OnDisable()
     {
         parryInputAction.Disable();
+    }
+
+   IEnumerator EndParry()
+    {
+        // Wait until the transition into Parry actually completes
+        while (!animator.GetCurrentAnimatorStateInfo(0).IsName("Parry"))
+            yield return null;
+
+        // Now wait until it finishes playing
+        while (animator.GetCurrentAnimatorStateInfo(0).IsName("Parry") &&
+            animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1f)
+            yield return null;
+
+        getParry = false;
     }
 }
