f058c44420bd7dcf96ae750a461b329cdb0544fd
Daniel BARSEGHIAN <barseghian.daniel@gmail.com>
2026-08-17 12:49:55 +0200

    added small tip when entering end without having enough kills

--- Kill-The-Monkey/Assets/Scripts/Finish.cs
+++ Kill-The-Monkey/Assets/Scripts/Finish.cs
@@ -1,23 +1,60 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Finish : MonoBehaviour
 {
     public EnemyKilled script;
     private LevelController levelHolder;
+    public TextMeshProUGUI enoughKills;
+
+    [Header("UI Settings")]
+    [Tooltip("How fast the text fades away. Higher = faster.")]
+    public float fadeSpeed = 0.5f; 
 
     void Start()
     {
-        // Find the Level Controller script
         levelHolder = GameObject.FindGameObjectWithTag("LevelController").GetComponent<LevelController>();
+
+        SetTextAlpha(0f);
+    }
+
+    void Update()
+    {
+        if (enoughKills.color.a > 0)
+        {
+            Color currentColor = enoughKills.color;
+            currentColor.a -= fadeSpeed * Time.deltaTime;
+            enoughKills.color = currentColor;
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (script.enemyKilled >= script.killRequired)
+        if (other.CompareTag("Player"))
         {
-            levelHolder.currentLevel++;
-            SceneManager.LoadScene(levelHolder.currentLevel);
+            if (script.enemyKilled >= script.killRequired)
+            {
+                levelHolder.currentLevel++;
+                SceneManager.LoadScene(levelHolder.currentLevel);
+            }
+            else
+            {
+                enoughKills.SetText($"You need {script.killRequired - script.enemyKilled} more kills");
+                SetTextAlpha(1f);
+            }
         }
+    }
+
+    private void SetTextAlpha(float targetAlpha)
+    {
+        // make a new color
+        Color newColor = enoughKills.color;
+
+        // Aplly the wanted alpha (transparancy)
+        newColor.a = targetAlpha;
+
+        // and set it
+        enoughKills.color = newColor;
     }
 }
